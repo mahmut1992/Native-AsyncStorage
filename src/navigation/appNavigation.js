2a391c8b55dc10598d9ae296b07e581ed3253ea1
seliinatmaca <selinatmacayy@gmail.com>
2024-10-12 15:42:07 +0300

    update

--- src/navigation/appNavigation.js
+++ src/navigation/appNavigation.js
@@ -9,13 +9,22 @@ import TodosScreen from '../screens/TodosScreen.jsx';
 const Stack = createNativeStackNavigator();
 
 export default function AppNavigation() {
+  //   showOnboarding, onboarding ekranını gösterip göstermeyeceğinizi kontrol eden bir state'dir.
+  // Başlangıç değeri null, çünkü henüz kullanıcının onboarding sürecini tamamlayıp tamamlamadığı bilinmiyor.
+  // setShowOnboarding, bu state'i güncellemek için kullanılan fonksiyondur.
   const [showOnboarding, setShowOnboarding] = useState(null);
+  //   useEffect hook'u, bileşen yüklendiğinde (ilk render) checkIfAlreadyOnboarded fonksiyonunu çağırır. Yani, uygulama başlatıldığında hemen kullanıcının onboarding sürecini tamamlayıp tamamlamadığını kontrol eder.
+  // Bu sadece bir kez çalışır ([] boş bağımlılık dizisi ile).
   useEffect(() => {
+    //Bu, kullanıcının onboarding sürecini tamamlayıp tamamlamadığını kontrol eden asenkron fonksiyondur.
     checkIfAlreadyOnboarded();
   }, []);
 
+  //   getItem('onboarded') fonksiyonu ile AsyncStorage'dan 'onboarded' anahtarıyla kayıtlı veriyi alır. Bu veriye göre kullanıcı onboarding ekranını daha önce görmüş mü görmemiş mi anlarız.
+  // Eğer kullanıcı onboarding sürecini daha önce tamamladıysa bu değer 1 olur.
   const checkIfAlreadyOnboarded = async () => {
     let onboarded = await getItem('onboarded');
+    //Eğer onboarded değeri 1 ise, yani kullanıcı onboarding'i tamamlamışsa:
     if (onboarded == 1) {
       // hide onboarding
       setShowOnboarding(false);
